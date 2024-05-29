e907f8d526b31a394f72f9c42c0a2acff3c14985
bhaviljain <bhaviljain26@gmail.com>
2024-05-30 00:48:19 +0530

    Data conversion

---
+++ DataConversion/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	num := 42
+	// num = num + 10 //can be added
+	fmt.Println(num)
+	fmt.Printf("Type %T", num)
+
+	num = 123
+	str := strconv.Itoa(num)
+	fmt.Println("str is", str)
+	fmt.Printf("type of %T\n", str)
+
+	number := "1234"
+	nums, _ := strconv.Atoi(number)
+	nums = nums + 100
+	fmt.Println("str is", nums)
+	fmt.Printf("type of %T\n", nums)
+
+	number1 := "1234.9011"
+	nums1, _ := strconv.ParseFloat(number1, 64)
+	fmt.Println("str is", nums1)
+	fmt.Printf("type of %T\n", nums1)
+}
