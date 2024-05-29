1319c6e79e6d6be8b203645b5a7e934114b98a6c
bhaviljain <bhaviljain26@gmail.com>
2024-05-30 00:53:09 +0530

    Dataconversion

--- DataConversion/main.go
+++ DataConversion/main.go
@@ -6,24 +6,24 @@ import (
 )
 
 func main() {
-	num := 42
-	// num = num + 10 //can be added
-	fmt.Println(num)
-	fmt.Printf("Type %T", num)
+	// num := 42
+	// // num = num + 10 //can be added
+	// fmt.Println(num)
+	// fmt.Printf("Type %T", num)
 
-	num = 123
-	str := strconv.Itoa(num)
-	fmt.Println("str is", str)
-	fmt.Printf("type of %T\n", str)
+	// num = 123
+	// str := strconv.Itoa(num)
+	// fmt.Println("str is", str)
+	// fmt.Printf("type of %T\n", str)
 
-	number := "1234"
-	nums, _ := strconv.Atoi(number)
-	nums = nums + 100
-	fmt.Println("str is", nums)
-	fmt.Printf("type of %T\n", nums)
+	// number := "1234"
+	// nums, _ := strconv.Atoi(number)
+	// nums = nums + 100
+	// fmt.Println("str is", nums)
+	// fmt.Printf("type of %T\n", nums)
 
-	number1 := "1234.9011"
-	nums1, _ := strconv.ParseFloat(number1, 64)
+	number1 := "-123"
+	nums1, _ := strconv.ParseInt(number1, 10, 64)
 	fmt.Println("str is", nums1)
 	fmt.Printf("type of %T\n", nums1)
 }
